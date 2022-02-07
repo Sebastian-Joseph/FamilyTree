a4f72797c7fea3f167424cf78bb0d29378fd5cbd
Sebastian-Joseph <theclimax25@gmail.com>
2022-02-07 10:38:50 -0500

    Changed a lot

--- src/BinaryFamilyTree.java
+++ src/BinaryFamilyTree.java
@@ -1,55 +1,27 @@
-public class BinaryFamilyTree {
+class Node {
+    String key;
+    Node left, right;
 
-    public static class Node {
-        String data;
-        Node left;
-        Node right;
+    public Node(String item) {
+        key = item;
+        left = right = null;
+    }
 
-        public Node(String data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
+}
 
-        }
+class BinaryTree {
+    Node root;
 
+    BinaryTree(String key) {
+        root = new Node(key);
+    }
 
-
-        public Node getLeft() {
-            return left;
-        }
-
-        public Node getRight() {
-            return right;
-        }
-
-        public static void run() {
-           Node root = new Node("Sebastian Joseph");
-            insert(root, "Momma Joseph");
-            insert(root, "Papa Joseph");
-
-        }
-
-        public static void insert(Node node, String data) {
-            if (data != node.data) {
-                if (node.left != null) {
-                    insert(node.left, data);
-                } else {
-                    System.out.println(" Inserted " + data + " to the left of Node " + node.data);
-                    node.left = new Node(data);
-                }
-            } else if (data != node.data) {
-                if (node.right != null) {
-                    insert(node.right, data);
-                } else {
-                    System.out.println(" Inserted " + data + " to the right of Node " + node.data);
-                    node.right = new Node(data);
-                }
-            }
-        }
-
+    BinaryTree() {
+        root = null;
     }
 
     public static void main(String[] args) {
-        Node.run();
+
     }
 }
+
