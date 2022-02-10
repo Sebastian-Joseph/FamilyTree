eafc89bb0b5f391441ac402441a80555e8b58a77
Sebastian-Joseph <theclimax25@gmail.com>
2022-02-10 17:27:17 -0500

    It is done.

--- src/BinaryFamilyTree.java
+++ src/BinaryFamilyTree.java
@@ -1,27 +1,84 @@
-class Node {
-    String key;
-    Node left, right;
+public class BinaryFamilyTree {
 
-    public Node(String item) {
-        key = item;
-        left = right = null;
-    }
 
-}
+    public static class Node {
+        String data;
+        Node left;
+        Node right;
+        int age;
 
-class BinaryTree {
-    Node root;
+        public Node(String data, int age) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+            this.age = age;
 
-    BinaryTree(String key) {
-        root = new Node(key);
-    }
+        }
 
-    BinaryTree() {
-        root = null;
+
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public static void run() {
+
+            Node root = new Node("Sebastian", 100);
+            insert(root, "Mom", 110);
+           insert(root, "Dad", 50);
+           insert(root, "Grandma (Mom)",111);
+           insert(root, "Grandpa (Mom)", 109);
+           insert(root, "Great-Grandma (Mom)", 112);
+           insert(root, "Great-Grandpa (Mom)", 108);
+           insert(root, "Grandma (Dad)", 51);
+           insert(root, "Grandpa (Dad)", 49);
+           insert(root, "Great-Grandma (Dad)", 52);
+           insert(root, "Great-Grandpa (Dad)", 48);
+
+
+            Node.traverseInOrder(root);
+
+
+        }
+
+
+
+        public static void insert(Node node, String data, int age) {
+
+            if (age < node.age) { if (node.left != null) { insert(node.left, data, age); } else { System.out.println(" Inserted " + data + " to left of " + node.data); node.left = new Node(data, age); } } else if (age > node.age) {
+                if (node.right != null) {
+                    insert(node.right, data, age);
+                } else {
+                    System.out.println("  Inserted " + data + " to right of "
+                            + node.data);
+                    node.right = new Node(data, age);
+                }
+            }
+
+
+        }
+
+        public static void traverseInOrder(Node node) {
+            if (node != null) {
+                traverseInOrder(node.left);
+                System.out.print(" " + node.data);
+                traverseInOrder(node.right);
+            }
+        }
+
     }
 
     public static void main(String[] args) {
+        BinaryFamilyTree tree = new BinaryFamilyTree();
 
+        Node.run();
     }
 }
-
