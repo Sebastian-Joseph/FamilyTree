342a7af74193332e8627d65d2a10bce91be81f7c
Sebastian-Joseph <theclimax25@gmail.com>
2022-02-11 10:20:28 -0500

    Formatting

--- src/BinaryFamilyTree.java
+++ src/BinaryFamilyTree.java
@@ -33,15 +33,23 @@ public class BinaryFamilyTree {
 
             Node root = new Node("Sebastian", 100);
             insert(root, "Mom", 110);
-           insert(root, "Dad", 50);
-           insert(root, "Grandma (Mom)",111);
-           insert(root, "Grandpa (Mom)", 109);
-           insert(root, "Great-Grandma (Mom)", 112);
-           insert(root, "Great-Grandpa (Mom)", 108);
-           insert(root, "Grandma (Dad)", 51);
-           insert(root, "Grandpa (Dad)", 49);
-           insert(root, "Great-Grandma (Dad)", 52);
-           insert(root, "Great-Grandpa (Dad)", 48);
+            insert(root, "Dad", 50);
+
+            insert(root, "Grandma (Mom)",111);
+            insert(root, "Grandpa (Mom)", 109);
+
+            insert(root, "Great-Grandma (Mom)", 112);
+            insert(root, "Great-Grandpa (Mom)", 108);
+
+
+
+
+            insert(root, "Grandma (Dad)", 51);
+            insert(root, "Grandpa (Dad)", 49);
+
+
+            insert(root, "Great-Grandma (Dad)", 52);
+            insert(root, "Great-Grandpa (Dad)", 48);
 
 
             Node.traverseInOrder(root);
@@ -57,14 +65,13 @@ public class BinaryFamilyTree {
                 if (node.left != null) {
                     insert(node.left, data, gen);
                 } else {
-                    System.out.println(" Inserted " + data + " to left of " + node.data); node.left = new Node(data, gen);
+                    System.out.println("Inserted " + data + " to left of " + node.data); node.left = new Node(data, gen);
                 }
             } else if (gen > node.gen) {
                 if (node.right != null) {
                     insert(node.right, data, gen);
                 } else {
-                    System.out.println("  Inserted " + data + " to right of "
-                            + node.data);
+                    System.out.println("Inserted " + data + " to right of " + node.data);
                     node.right = new Node(data, gen);
                 }
             }
@@ -73,14 +80,18 @@ public class BinaryFamilyTree {
         }
 
         public static void traverseInOrder(Node node) {
-            if (node != null) {
-                traverseInOrder(node.left);
-                System.out.print(" " + node.data);
-                traverseInOrder(node.right);
+            if (node == null) {
+                return;
             }
+
+            traverseInOrder(node.left);
+            System.out.printf("%s ", node.data);
+            traverseInOrder(node.right);
         }
 
-    }
+        }
+
+
 
     public static void main(String[] args) {
         BinaryFamilyTree tree = new BinaryFamilyTree();
